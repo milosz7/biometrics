b0ff1cd1003721d5eb3ed9459407879b8efca9e1
miloszbargiel <miloszbargiel@gmail.com>
2026-04-19 19:32:11 +0200

    Add embedding model

---
+++ project/embedding_model.py
@@ -0,0 +1,18 @@
+from insightface.app import FaceAnalysis
+
+
+class EmbeddingModel:
+    def __init__(self, config):
+        providers = ["CoreMLExecutionProvider", "CPUExecutionProvider"]
+        self.model = FaceAnalysis(
+            name=config.FACE_ANALYSIS_MODEL,
+            providers=providers,
+            allowed_modules=config.FACE_ANALYSIS_MODEL_MODULES,
+        )
+        self.model.prepare(ctx_id=-1)
+
+    def __call__(self, x):
+        result = self.model.get(x)[0]
+        return result["embedding"]
+
+
